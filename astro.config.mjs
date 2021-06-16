f7eb37295a5a8dca6c72227485f83f8c2e9118f9
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-06-16 15:42:12 +0530

    Add domain for sitemap and canonical URL generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ export default {
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    // sitemap: true,      // Generate sitemap (set to "false" to disable)
+    site: 'https://astro-note.vercel.app', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: true, // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
     // port: 3000,         // The port to run the dev server on.
